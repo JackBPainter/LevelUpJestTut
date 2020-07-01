76e2377c5180cca44c2f11cec7fe0804b9518e40
Jack Painter <littlesympathy@jacks-mbp.home>
2020-07-01 15:51:22 +0100

    Create first integration test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { add } from './app'
+import { add, total } from './app'
 
 
 // Below is a Unit Test (Because it only tests one thing) 
@@ -6,6 +6,13 @@ import { add } from './app'
 test("add", () => {
     const value = add(1, 2)
     expect(value).toBe(3)
+})
+
+// Below is an Integration Test (Because it relies on multiple functions in order to work)
+
+test("total", () => {
+    const value = total(5, 15)
+    expect(value).toBe("$20")
 })
 
 // Test functions have a name (first argument) and the second argument is an assertion
