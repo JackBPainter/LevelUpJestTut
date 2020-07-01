79f4d29f2593b0f3ed1d646aae1acd690edd5556
Jack Painter <littlesympathy@jacks-mbp.home>
2020-07-01 14:56:45 +0100

    Create first real test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,11 @@
-test('fake test', () => {
-    expect(true).toBeTruthy()
+import { add } from './app'
+
+
+// Below is a Unit Test (Because it only tests one thing) 
+
+test("add", () => {
+    const value = add(1, 2)
+    expect(value).toBe(3)
 })
 
 // Test functions have a name (first argument) and the second argument is an assertion
